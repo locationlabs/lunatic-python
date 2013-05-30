baa26b0a91fb0a9b7f85739f575af6b0f58dc380
Gaurav Kumar Gupta <gaurav.kumar@locationlabs.com>
2013-05-30 14:58:08 +0530

    1. Changed package name to lunatic-python-bugfix 2. Changed version to 1.1 to reflect it as a bugfix over version 1.0 in https://pypi.python.org/pypi/lunatic-python 3. Updated description to reflect that its a bugfix 4. Fixed some formatting to make it pep8 compliant.
    bug:SS-2029

--- setup.py
+++ setup.py
@@ -46,25 +46,27 @@ def pkgconfig(*packages):
 
 lua_pkgconfig = pkgconfig('lua', 'lua' + LUAVERSION)
 
-setup(name="lunatic-python",
-      version="1.0",
+setup(name="lunatic-python-bugfix",
+      version="1.1",
       description="Two-way bridge between Python and Lua",
       author="Gustavo Niemeyer",
       author_email="gustavo@niemeyer.net",
-      url="http://labix.org/lunatic-python",
+      url="https://github.com/locationlabs/lunatic-python",
       license="LGPL",
       long_description="""\
+Includes bugfixes on the original https://pypi.python.org/pypi/lunatic-python
+ module.
 Lunatic Python is a two-way bridge between Python and Lua, allowing these
 languages to intercommunicate. Being two-way means that it allows Lua inside
 Python, Python inside Lua, Lua inside Python inside Lua, Python inside Lua
 inside Python, and so on.
 """,
       ext_modules=[
-        Extension("lua-python",
+          Extension("lua-python",
                   ["src/pythoninlua.c", "src/luainpython.c"],
                   **lua_pkgconfig),
-        Extension("lua",
-                  ["src/pythoninlua.c", "src/luainpython.c"],
-                  **lua_pkgconfig),
-        ],
+          Extension("lua",
+                    ["src/pythoninlua.c", "src/luainpython.c"],
+                    **lua_pkgconfig),
+      ],
       )
